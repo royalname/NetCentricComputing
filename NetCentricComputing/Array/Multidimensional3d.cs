c3ef5c35aed5e12ef69eaa8da0f837d51bd1b3f9
Royal Shrestha <royalshrestha003@gmail.com>
2026-07-14 10:50:47 +0545

    Add project files.

---
+++ NetCentricComputing/Array/Multidimensional3d.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace NetCentricComputing.Array
+{
+    internal class multidimensionalArray3D
+    {
+        public static void Run()
+        {
+            int[,,] numbers =
+            {
+                {
+                    {1, 2, 3},
+                    {4, 5, 6}
+                },
+                {
+                    {7, 8, 9},
+                    {10, 11, 12}
+                }
+            };
+
+            Console.WriteLine("3D Multidimensional Array:");
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                Console.WriteLine($"Layer {i}");
+
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    for (int k = 0; k < numbers.GetLength(2); k++)
+                    {
+                        Console.Write(numbers[i, j, k] + " ");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ NetCentricComputing/Program.cs
@@ -0,0 +1,17 @@
+﻿using NetCentricComputing.Array;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCentricComputing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            multidimensionalArray3D.Run();
+        }
+    }
+}
